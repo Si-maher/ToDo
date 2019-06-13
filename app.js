06a444ded37cf683a740869783761d3b69650ce9
Simon Maher <simon.maher@integrify.io>
2019-06-13 17:05:14 +0300

    Created search function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add')
 const list = document.querySelector('.todos')
+const search = document.querySelector('.search input')
 const generateTemplate = (todo) => {
     const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -27,4 +28,20 @@ list.addEventListener('click', e => {
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.remove()
     }
+})
+
+// Function that filters todo list
+
+const filterTodo = (term) => {
+    Array.from(list.children).filter((todo) => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'))
+
+    Array.from(list.children).filter((todo) => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.remove('filtered'))
+}
+// keyup event
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase()
+    filterTodo(term)
 })
